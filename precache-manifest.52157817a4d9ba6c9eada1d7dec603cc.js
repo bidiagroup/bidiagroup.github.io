e57c50d9b5fef68b270ada410ccb31a8eb570af1
Boubacar Tamboura <tamboura23@gmail.com>
2021-02-10 05:23:22 +0100

    Init repo

---
+++ precache-manifest.52157817a4d9ba6c9eada1d7dec603cc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66dfaf813f067349f4dd",
+    "url": "/css/app.2c7c90ca.css"
+  },
+  {
+    "revision": "433846be99b533eb760a",
+    "url": "/css/chunk-vendors.0d9171ac.css"
+  },
+  {
+    "revision": "ca8b43c8d5c8ce977d715df0e3349512",
+    "url": "/fonts/Charm-Regular.ca8b43c8.ttf"
+  },
+  {
+    "revision": "04557065a426bd218b5acde9ae3c4695",
+    "url": "/fonts/ElMessiri-Regular.04557065.ttf"
+  },
+  {
+    "revision": "27f813cadd3a3af891e01acf64272748",
+    "url": "/fonts/Handlee-Regular.27f813ca.ttf"
+  },
+  {
+    "revision": "eeb4dad66cf2ff3ee47b7ac7906913a0",
+    "url": "/fonts/PatrickHand-Regular.eeb4dad6.ttf"
+  },
+  {
+    "revision": "908041eb55f49ddaf5c641650dd52004",
+    "url": "/img/Africa.908041eb.png"
+  },
+  {
+    "revision": "b8d0b74bc69bde5cca3cf0be56d20ea7",
+    "url": "/img/LOGO_BIDIA_GROUP_DARK_ORANGE.b8d0b74b.png"
+  },
+  {
+    "revision": "e467e88aba7bc3edcbabb2421e5c9948",
+    "url": "/img/earth.e467e88a.jpg"
+  },
+  {
+    "revision": "6dc9c8e3a18f666fb34a42e3372fd16c",
+    "url": "/img/green-plant.6dc9c8e3.jpg"
+  },
+  {
+    "revision": "796207933886a37d5bc830e755d2b4de",
+    "url": "/img/sunset-baobabs.79620793.jpg"
+  },
+  {
+    "revision": "b2c5df0ee3b28872029ac8dd6e1a87f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66dfaf813f067349f4dd",
+    "url": "/js/app.59116c24.js"
+  },
+  {
+    "revision": "433846be99b533eb760a",
+    "url": "/js/chunk-vendors.47d6f524.js"
+  },
+  {
+    "revision": "073101bf310ef2b1dcc237049609733c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
